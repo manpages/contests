901056d136bc5b05671de0344d9ed742230b4260
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-07-21 23:35:43 +0300

    formatted according to http://neogfx.org/styleguide.htm

--- dailyprogramming/79/easy.cpp
+++ dailyprogramming/79/easy.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <algorithm>
 
-void step_count(const float argFltAlpha, const float argFltBeta, const int argIntSteps, float* argFltPtrState) {
-    // Init. result array with argFltBeta as the value
-    std::fill_n(argIntPtrState, argIntSteps, argFltBeta);
+void step_count(const float aAlpha, const float aBeta, const int aSteps, float* aState) {
+    // Init. result array with aBeta as the value
+    std::fill_n(aState, aSteps, aBeta);
     // Solve the task
-    if (argIntSteps >= 2) {
+    if (aSteps >= 2) {
         // Calculate the step
-        float locFltStep = (argFltBeta-argFltAlpha)/(argIntSteps-1);
+        float step = (aBeta-aAlpha)/(aSteps-1);
         // Solve the bich and store the data in the last argument
-        for (int i=argIntSteps; i>=0; --i) {
-            argIntPtrState[(i-2)] = argIntPtrState[(i-1)] - locFltStep;
+        for (int i=aSteps; i>=0; --i) {
+            aState[(i-2)] = aState[(i-1)] - step;
         }
     }
     return;
 }
 
 int main() {
-    float locIntPtrResult[5];
-    step_count(18.75, -22.0, 5, locIntPtrResult);
+    float result[5];
+    step_count(18.75, -22.0, 5, result);
     std::cout << "[ ";
-    for (int locI = 0; locI < 5; locI++) std::cout << locIntPtrResult[locI] << ", ";
+    for (int i = 0; i < 5; i++) std::cout << result[i] << ", ";
     std::cout << "]";
     return 0;
 }
